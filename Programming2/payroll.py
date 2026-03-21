dd7458658cfce6f1dcf89e04ff5a9884af71477d
Mio <kari.bellaros@gmail.com>
2026-03-21 02:05:34 -0700

    nn

---
+++ Programming2/payroll.py
@@ -0,0 +1,32 @@
+import data
+
+def calculate_payroll():
+    if not data.employees:
+        print("Load employees first!")
+        return
+
+    for worker in data.employees:
+
+        # Only process ACTIVE employees
+        if worker["Status"] != "Active":
+            continue
+
+        try:
+            hours = float(input(f"Enter hours for {worker['FullName']}: "))
+        except ValueError:
+            print("Invalid input. Skipping employee.")
+            continue
+
+        rate = float(worker["HourlyRate"])
+
+        gross = hours * rate
+        nis = gross * 0.025
+        edu_tax = (gross - nis) * 0.0225
+        net = gross - nis - edu_tax
+
+        print("\n--- Payroll Result ---")
+        print(f"Name: {worker['FullName']}")
+        print(f"Gross Pay: {gross:.2f}")
+        print(f"NIS: {nis:.2f}")
+        print(f"Education Tax: {edu_tax:.2f}")
+        print(f"Net Pay: {net:.2f}")
